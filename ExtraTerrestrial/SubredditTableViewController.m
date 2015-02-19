99e33648192050b8a3a592f6866ac66414b7d16a
plsr <christianpoplawski@gmail.com>
2015-02-19 15:04:42 +0100

    Documentation

--- ExtraTerrestrial/SubredditTableViewController.m
+++ ExtraTerrestrial/SubredditTableViewController.m
@@ -1,5 +1,5 @@
 //
-//  ForntPageTableViewController.m
+//  SubredditTableViewController.m
 //  ExtraTerrestrial
 //
 //  Created by Christian Poplawski on 10/02/15.
@@ -8,12 +8,14 @@
 
 #import "SubredditTableViewController.h"
 
+static NSString * const kSelfPostTableIdentifier = @"selfPostTableCell";
+static NSString * const kLinkPostTableIdentifier = @"linkPostTableViewCell";
+
 @interface SubredditTableViewController ()
 {
+    // TODO: Declare in header?
     NSURL *frontpageURL;
     NSArray *tableContents;
-    NSArray *test;
-
 }
 
 
@@ -31,10 +33,8 @@
     // Initialize table data
     frontpageURL = [NSURL URLWithString:@"http://reddit.com/.json"];
     self.apiCall = [[RedditAPICall alloc] initWithURL:frontpageURL];
-    
     NSArray *keys = [[NSArray alloc] initWithObjects:@"title", @"subreddit", @"score", @"num_comments", @"thumbnail", @"domain", @"permalink", @"is_self", @"selftext", @"author", nil];
     tableContents = [self.apiCall contentOfChildrenForKeys:keys];
-    //test = [tableContents objectForKey:@"thumbnail"];
     
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -51,33 +51,48 @@
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
+    // Subreddit views always have just one section.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
     // Return the number of rows in the section.
     return [tableContents count];
 }
 
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+#pragma mark - Fill table with data
 
-    static NSString *selfPostTableIdentifier = @"selfPostTableCell";
-    static NSString *linkPostTableIdentifier = @"linkPostTableViewCell";
+
+/*
+ *  Fills the cell at the given indexPath with content.
+ *  There are 2 different types of cells:
+ *      1. Posts leading to a self.subreddit posts. Those do not contain an UIImageView
+ *      2. Post linking to a 3rd-Party website. Those do _always_ have an UIImageView
+ *  For link posts, the function checks if there is a thumbnail in the post (e.g. for imgur, 
+ *  youtube and some "text-links") or if the post has to be set up with a default image.
+ *  
+ *  NOTE: It's a design decision to set up _all_ link-posts with an UIImage to make it easier
+ *  for the user to scan over the overview.
+ */
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
+    // is self post?
     if([self isSelfPost:indexPath.row]) {
-        SubredditTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:selfPostTableIdentifier];
+        // set cell up
+        SubredditTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kSelfPostTableIdentifier];
         [self configureSelfPostCell:cell atIndexPath:indexPath];
         return cell;
     } else {
-        SubredditTableViewImageCell *cell = [tableView dequeueReusableCellWithIdentifier:linkPostTableIdentifier];
+        SubredditTableViewImageCell *cell = [tableView dequeueReusableCellWithIdentifier:kLinkPostTableIdentifier];
+        // has thumbnail?
         if([self hasImageAtIndexPath:indexPath.row]) {
+            // set cell up with given thumbnail
             [self configureLinkPostCellWithThumbnail:cell atIndexPath:indexPath];
             return cell;
         } else {
+            // set cell up with default thumbnail
             [self configureLinkPostCellWithPlaceholder:cell atIndexPath:indexPath];
             return cell;
         }
@@ -86,6 +101,9 @@
 }
 
 
+/*
+ *  Configures a self.subreddit cell. Sets the title, the subreddit and the domain.
+ */
 - (void)configureSelfPostCell:(SubredditTableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
     cell.titleLabel.text = [[tableContents objectAtIndex:indexPath.row] valueForKey:@"title"];
     cell.subredditLabel.text = [[tableContents objectAtIndex:indexPath.row] valueForKey:@"subreddit"];
@@ -93,6 +111,9 @@
 }
 
 
+/*
+ *  Sets up a link-post cell with a given thumbnail.
+ */
 - (void)configureLinkPostCellWithThumbnail:(SubredditTableViewImageCell *)cell atIndexPath:(NSIndexPath *)indexPath {
     cell.titleLabel.text = [[tableContents objectAtIndex:indexPath.row] valueForKey:@"title"];
     cell.subredditLabel.text = [[tableContents objectAtIndex:indexPath.row] valueForKey:@"subreddit"];
@@ -101,6 +122,9 @@
 }
 
 
+/*
+ *  Sets up a link-post cell with the default thumbnail.
+ */
 - (void)configureLinkPostCellWithPlaceholder:(SubredditTableViewImageCell *)cell atIndexPath:(NSIndexPath *)indexPath {
     cell.titleLabel.text = [[tableContents objectAtIndex:indexPath.row] valueForKey:@"title"];
     cell.subredditLabel.text = [[tableContents objectAtIndex:indexPath.row] valueForKey:@"subreddit"];
@@ -110,12 +134,25 @@
 
 
 
+#pragma mark - Helper methods
+
+/*
+ *  Checks if the post at the given index does contain a thumbnail.
+ */
 -(BOOL) hasImageAtIndexPath: (NSUInteger) indexOfPost {
     if ([[tableContents objectAtIndex:indexOfPost] objectForKey:@"thumbnail"]) {
         return YES;
     } else {
         return NO;
     }
+}
+
+
+/*
+ *  Checks if the post at the given index of the posts array is a self.subreddit post.
+ */
+-(BOOL) isSelfPost: (NSUInteger) indexOfPost  {
+    return [[[tableContents objectAtIndex:indexOfPost] objectForKey:@"is_self"] boolValue];
 }
 
 /*
@@ -176,11 +213,6 @@
 }
 
 
-/*
- *  Checks if the post at the given index of the posts array is a self.subreddit post.
- */
--(BOOL) isSelfPost: (NSUInteger) indexOfPost  {
-    return [[[tableContents objectAtIndex:indexOfPost] objectForKey:@"is_self"] boolValue];
-}
+
 
 @end
